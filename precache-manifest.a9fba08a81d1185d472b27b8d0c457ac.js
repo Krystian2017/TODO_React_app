f001fc96f23a7efd2b82b9f27fae2846555f12fe
Krystian Bieniek <krystekk2013@gmail.com>
2019-10-16 21:12:14 +0200

    Updates

---
+++ precache-manifest.a9fba08a81d1185d472b27b8d0c457ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20d5f8befd529cdccff0",
+    "url": "/TODO_React_app/static/js/runtime~main.20d5f8be.js"
+  },
+  {
+    "revision": "a90f7c47b0065bee6d35",
+    "url": "/TODO_React_app/static/js/main.a90f7c47.chunk.js"
+  },
+  {
+    "revision": "d4e2edbd0fdedb0a1f7a",
+    "url": "/TODO_React_app/static/js/1.d4e2edbd.chunk.js"
+  },
+  {
+    "revision": "a90f7c47b0065bee6d35",
+    "url": "/TODO_React_app/static/css/main.e41e20b1.chunk.css"
+  },
+  {
+    "revision": "55de34e9ed928366a36b4e4511a84151",
+    "url": "/TODO_React_app/index.html"
+  }
+];
